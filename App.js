c42c910fd10f450f102281f01854e3407507df4b
Crackita <91308668+Crackita@users.noreply.github.com>
2022-11-10 09:13:07 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import { Counter } from "assets"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>UE</Text>
+            <TouchableHighlight
+                activeOpacity={0.4}
+                underlayColor="pink"
+                onPress={() => alert("Pressed !")}
+            >
+                <Text style={styles.text}>test !</Text>
+            </TouchableHighlight>
+            <Counter />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        color: "blue",
+        fontSize: 50,
+    },
 });
